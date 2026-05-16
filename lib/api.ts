501790f35fbae442d0c15a6d020015d8411b8f6a
SashaOsp <sasha.mykol@gmail.com>
2026-05-16 13:25:25 +0300

    swap

--- lib/api.ts
+++ lib/api.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 import type { Note, NewNote } from "../types/note";
 import { NoteList } from "@/types/noteList";
 
-const NOTEHUB_TOKEN = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
-
 axios.defaults.baseURL = "https://notehub-public.goit.study/api/";
+
+const NOTEHUB_TOKEN = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
 
 export const fetchNotes = async (
   note: string,
